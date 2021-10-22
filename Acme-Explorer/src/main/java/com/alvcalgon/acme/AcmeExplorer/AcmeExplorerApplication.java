d89ba722b73da9b53bcb0af60e99d685db61d47c
alvarocallegonzalez <alvarogoles@gmail.com>
2021-10-22 18:05:17 +0200

    feat: Proyecto plantilla

---
+++ Acme-Explorer/src/main/java/com/alvcalgon/acme/AcmeExplorer/AcmeExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.alvcalgon.acme.AcmeExplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmeExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcmeExplorerApplication.class, args);
+	}
+
+}
---
+++ Acme-Explorer/src/test/java/com/alvcalgon/acme/AcmeExplorer/AcmeExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvcalgon.acme.AcmeExplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmeExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
